5680cd6695dae17c6606e0ab88e27f952cb53a31
Zen <zen.mycena@gmail.com>
2016-10-12 15:19:05 +0800

    Change json data property name

--- typhoon-stop-work-spider.py
+++ typhoon-stop-work-spider.py
@@ -9,23 +9,23 @@ from kafka import KafkaProducer
 from kafka.common import KafkaError
 
 class TyphoonJson(object):
-    def __init__(self, updated, announcement_list):
+    def __init__(self, updated, disaster_declarations):
         self.updated = updated
-        self.announcement_list = announcement_list
-        
+        self.disaster_declarations = disaster_declarations
+
 class TyphoonMessage(object):
-    def __init__(self, county, announced):
-        self.county = county
-        self.announced = announced
-    
+    def __init__(self, city, content):
+        self.city = city
+        self.content = content
+
 class TyphoonStopWorkSpider(scrapy.Spider):
-  
+
     name = 'typhoonstopworkspider'
-    start_urls = ['http://www.dgpa.gov.tw/nds.html']    
+    start_urls = ['http://www.dgpa.gov.tw/nds.html']
     KAFKA_SERVER_IP = '59.127.187.54'
     KAFKA_SERVER_PORT = '9092'
-    KAFKA_TOPIC = 'typhon_dayoff_announcement'
-    
+    KAFKA_TOPIC = 'typhon_dayoff_declare'
+
     UPDATE_FILE_LOG = './update_time.txt'
 #    producer = KafkaProducer(bootstrap_servers=['59.127.187.54:9092'])
 
@@ -53,14 +53,14 @@ class TyphoonStopWorkSpider(scrapy.Spider):
             self.save_update_time(parse_update_time.extract()[2].encode('utf-8'))
             self.parse_content(body_content, parse_update_time.extract()[2].encode('utf-8'));
 
-    #儲存更新時間        
+    #儲存更新時間
     def save_update_time(self,update_time):
         text_file = open(self.UPDATE_FILE_LOG,"w")
         text_file.write(update_time)
         text_file.close()
         print 'Update time saved.'
 
-    #解析內文資訊    
+    #解析內文資訊
     def parse_content(self,body_content,update_time):
         #解析表格Title
         parse_body_content = Selector(text=body_content).xpath('//table/tbody/tr/td/font/h2/text()')
@@ -90,18 +90,18 @@ class TyphoonStopWorkSpider(scrapy.Spider):
         return o.__dict__
 
     def parse_to_json(self,parse_detail,update_time):
-        
+
 #        print parse_detail
-        
+
         parse_detail_list = parse_detail.split(';')
-        
+
 #        刪除資料來源字串
         del parse_detail_list[0]
 #        刪除空字串
         del parse_detail_list[1]
 
         dayoff_list = []
-        
+
         for index in range(len(parse_detail_list)):
             if index !=0 and index%2 == 1:
                 location = parse_detail_list[index]
@@ -109,20 +109,20 @@ class TyphoonStopWorkSpider(scrapy.Spider):
                 dayoff_list.append(TyphoonMessage(location, parse_detail_list[index].replace(' ','').replace(",","")))
 
         #日期處理
-        date = update_time.split('：')[1].replace(" ","");    
+        date = update_time.split('：')[1].replace(" ","");
 
         typhoonJsonObj = TyphoonJson(time.mktime(time.strptime(date , "%Y/%m/%d%H:%M:%S")),dayoff_list)
 
         typhoonJson = json.dumps(typhoonJsonObj, default=self.jdefault , ensure_ascii=False, indent = 4)
 #        print typhoonJson
         self.sendMsgToKafka(typhoonJsonObj)
-        
+
     def sendMsgToKafka(self, typhoonJsonObj):
 #        print typhoonJson.encode('utf-8')
         producer = KafkaProducer(bootstrap_servers=[self.KAFKA_SERVER_IP + ":" + self.KAFKA_SERVER_PORT], value_serializer=lambda v: json.dumps(v,default=self.jdefault , ensure_ascii=False).encode('utf-8'))
 #        producer.send('test', {'foo':'bar'})
 #        producer = KafkaProducer(bootstrap_servers=['59.127.187.54:9092'])
-        future = producer.send('typhon_dayoff_announcement', typhoonJsonObj)
+        future = producer.send('typhon_dayoff_declare', typhoonJsonObj)
         # Block for 'synchronous' sends
         try:
             record_metadata = future.get(timeout=10)
